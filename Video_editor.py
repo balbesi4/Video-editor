08b74f9ef95e50174fe2c8228b732af43cd2d7d3
Arseniy Yolkin <arseniy.yolkin@yandex.ru>
2023-08-08 17:28:35 +0500

    nu teper to4no pre-release

--- Video_editor.py
+++ Video_editor.py
@@ -1,11 +1,5 @@
-import imageio
-import visvis as vv
-from Fragment import Fragment
 from GUI import GraphicalUserInterface
 import atexit
-from TimeLine import TimeLine
-import moviepy.editor as mp
-import moviepy.video.fx.all as vfx
 
 
 gui = GraphicalUserInterface()
